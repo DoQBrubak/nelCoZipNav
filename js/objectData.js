3d054b917beac89a9eeae2e3ae5679e9869ccf42
DQB2w <quintin.brubaker@gmail.com>
2014-11-12 17:00:26 -0500

    In the midst of migrating HTML-generating stuff into objectData.

---
+++ js/objectData.js
@@ -0,0 +1,45 @@
+var objSource = {
+	"base": {
+		"val": "base",
+		"xy": [ 0, 0],
+		"fullname": "All of Nelson County"
+	},
+	"afto": {
+		"val": "afto",
+		"xy": [ 730, 3],
+		"fullname": "Afton",
+		"zip": "22920"
+	},
+	"amhe": {
+		"val": "amhe",
+		"xy": [ 1, 1005],
+		"fullname": "Amherst",
+		"zip": "24521"
+	},
+	"arri": {"val": "arri", "xy": [ 313, 1109],
+		"fullname": "Arrington",
+		"zip": "22922"
+	},
+	"buck": {"val": "buck", "xy": [ 1316, 1250],
+		"fullname": "Buckingham",
+		"zip": "23921"
+	},
+	"fabe": {"val": "fabe", "xy": [ 845, 465],
+		"fullname": "Faber",
+		"zip": "22938"
+
+	},
+	"glad": {"val": "glad", "xy": [ 510, 1510]},
+	"lovi": {"val": "lovi", "xy": [ 617, 722]},
+	"mont": {"val": "mont", "xy": [ 0, 545]},
+	"nell": {"val": "nell", "xy": [ 710, 370]},
+	"norw": {"val": "norw", "xy": [ 885, 1365]},
+	"pine": {"val": "pine", "xy": [ 435, 1080]},
+	"rose": {"val": "rose", "xy": [ 1, 375]},
+	"schu": {"val": "schu", "xy": [ 1170, 610]},
+	"ship": {"val": "ship", "xy": [ 855, 770]},
+	"tyro": {"val": "tyro", "xy": [ 110, 575]},
+	"vesu": {"val": "vesu", "xy": [ 0, 475]},
+	"wing": {"val": "wing", "xy": [ 1050, 1220]},
+	"wint": {"val": "wint", "xy": [ 0, 0]}
+};
