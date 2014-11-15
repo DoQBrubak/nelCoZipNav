e2a58464904fd4820764bc685880ccfa22566d54
DQB2w <quintin.brubaker@gmail.com>
2014-11-14 21:27:55 -0500

    Better now

--- js/buildPage.js
+++ js/buildPage.js
@@ -3,25 +3,6 @@ var HTMLforOption = "<option value='%needValue%'>%needDesc%</option>";
 var c = document.getElementById( 'map_canvas' );
 var ctx = c.getContext('2d');
 
-
-places.renderHTML = function() {
-  for (var i = 0; i <  placeKeys.length; i++) {
-    var useHTML = HTMLforOption;
-    useHTML = useHTML.replace('%needValue%', placeKeys[i]);
-    useHTML = useHTML.replace('%needDesc%',
-      this[placeKeys[i]].zip + " - " + this[placeKeys[i]].fullname);
-    $( '#towns_selector' ).append(useHTML);
-    console.log(useHTML)
-}};
-places.renderHTML();
-
-
-$( 'select' ).change( reTown );
-
-$( '#towns_selector' ).attr( 'value', 'base' );
-
-makeImgAndDraw( 'base' );
-
 //pass in a 4-letter name string
 var makeImgAndDraw = function( choosePlace ) {
   var localImgObj = new Image()
@@ -35,8 +16,26 @@ var makeImgAndDraw = function( choosePlace ) {
       places[choosePlace][ 'xy' ][1]
 )}};
 
+places.renderHTML = function() {
+  for (var i = 0; i <  placeKeys.length; i++) {
+    var useHTML = HTMLforOption;
+    useHTML = useHTML.replace('%needValue%', placeKeys[i]);
+    useHTML = useHTML.replace('%needDesc%',
+      this[placeKeys[i]].zip + " - " + this[placeKeys[i]].fullname);
+    $( '#towns_selector' ).append(useHTML);
+    console.log(useHTML)
+}};
+places.renderHTML();
+
+$( 'select' ).change( reTown );
+
+$( '#towns_selector' ).attr( 'value', 'base' );
+
+
 function reTown() {
   current = $( '#towns_selector' ).prop( 'value' );
   makeImgAndDraw( 'base' );
   makeImgAndDraw(current);
 };
+
+makeImgAndDraw('base');
